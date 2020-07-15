d56643bfb273d22dced6bf684387c6d7adeb22ac
osori <hyeseong43@gmail.com>
2020-07-15 13:24:40 +0900

    Part3.Shipping Address
    step1 Shipping Method

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Customer(models.Model):
 class Product(models.Model):
   name = models.CharField(max_length=200, null=True)
   price = models.FloatField()
-  digital = models.BooleanField(default=False, null=True, blank=True)
+  digital = models.BooleanField(default=False, null=True, blank=False)
   image = models.ImageField(null=True, blank=True)
   
   def __str__(self):
@@ -36,6 +36,15 @@ class Order(models.Model):
 
   def __str__(self):
     return str(self.id)
+
+  @property
+  def shipping(self):
+    shipping = False
+    orderitems = self.orderitem_set.all()
+    for i in orderitems:
+      if i.product.digital ==False:
+        shipping = True
+      return shipping
 
   @property
   def get_cart_total(self):
