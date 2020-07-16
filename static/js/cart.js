651b0378261318b691de4f8a6e8950b3ea1fab3f
osori <hyeseong43@gmail.com>
2020-07-16 14:13:01 +0900

    IV.Guest Checkout - Part1_Set Cookies
    Step_1 Create Cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,41 +1,37 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i=0; i<updateBtns.length; i++) {
-  updateBtns[i].addEventListener('click', function(){
-    var productId=this.dataset.product
-    var action=this.dataset.action
-    console.log('productId', productId, 'Action:', action)
-  
-    console.log('USER:', user)
-    if(user == 'AnonymouUser'){
-      console.log('User is not authenticated')
-    }else {
-      updateUserOrder(productId, action)
-    }
-  })
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
+
+		if (user == 'AnonymousUser'){
+			console.log('User is not authenticated')
+
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
 
 function updateUserOrder(productId, action){
-  console.log('User is logged in, sending data..')
+	console.log('User is authenticated, sending data...')
 
-  var url = '/update_item/'
+		var url = '/update_item/'
 
-  console.log('URL:', url)
-  
-  fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type':'application/json',
-        'X-CSRFToken': csrftoken,
-      },
-      body:JSON.stringify({'productId':productId, 'action':action})
-  })
-
-  .then((response)=>{
-    return response.json()
-  })
-
-  .then((data)=>{
-    location.reload()
-  })
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    console.log('Data:', data)
+		});
 }
